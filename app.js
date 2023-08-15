860f9740daed70ae4c8f34109d9495b42d0fe05a
Rodolfo Hercule <71905257+rodolfohercules@users.noreply.github.com>
2023-08-15 18:06:48 -0300

    fristCommit

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+
+
+Eu escolhi o Vs Code, pois ele tem diversas extensões para aprimorar a velocidade e usabilidade na hora da edição do código:
+
+ 
+ Ele tem autocomplete para auxiliar na escrita do código; 
+
+Ele possui um console nele mesmo; 
+
+Posso customizar do jeito que quero, com fonte, tema e afins. 
+
+E é um ótimo editor para JAVASCRIPT. 
+
+
+
+
+*/
+
+
+
+function somar(a, b) {
+    return a + b;
+}
+
+
+let resultado = somar(5, 3);
+
+somar()
+
+console.log(resultado);
+
+
+/* Criei uma função com nome de soma, passei os parâmetros (a, b), fiz ela retorna o valor de a + b. 
+Criei uma variável para armazenar o resultado da soma e invoquei a soma, depois a imprimi na tela. No caso usei o teste com node, por isso não tem arquivo HTML anexado. */ 
